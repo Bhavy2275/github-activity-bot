bcac78cf8d489038864a022327eea9d6398c95c7
Bhavy2275 <Bhavy2275@users.noreply.github.com>
2026-01-05 19:08:04 +0000

    feat(utils): implement secure hash calculation

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,8 @@
 # utils.py module
 
+
+def calculate_hash(data: str) -> str:
+    """Calculates a simple hash of the input string."""
+    import hashlib
+    return hashlib.sha256(data.encode()).hexdigest()
+
